620a40be114549ac117f0fb8e0fe1768c4609699
Андрей LightFall <krivosheevandrey93@gmail.com>
2019-05-03 00:56:41 +0300

    22

--- Audio_player/Player.cs
+++ Audio_player/Player.cs
@@ -13,17 +13,18 @@ namespace Audio_player
 
         private int volume;
         public int step;
-        private  bool playing;
+        private bool playing;
         public   bool Locked = false;
         public const int maxVolume = 300;
         public const int minVolume = 300;
-        public  Song[] Songs;
+        //public  Song[] Songs;
 
         public int Duration;
         public string Genre;
         public string Lyrics; 
         public string Path;
 
+        public List<Song> songs = new List<Song>();
 
         public string Title;
         
@@ -119,78 +120,57 @@ namespace Audio_player
             return playing;
         }
 
-        public  void Play(bool loop,ArrayList songs)
+
+        public List<Song> AddSongs()
         {
-            Add2();
-            //все ещё не работает return
+            songs.Add(new Song { Title = "1 eye of a tiger", Duration = 500, Genre = "rock", Lyrics = "lalalalalaalalalala" });
+            songs.Add(new Song { Title = "2 smooth criminal", Duration = 500, Genre = "pop", Lyrics = "lalalalalaalalalala" });
+            songs.Add(new Song { Title = "3 Kickapoo", Duration = 260, Genre = "rock", Lyrics = "lalalalalaalalalala" });
+            songs.Add(new Song { Title = "4 Гладкие Криминал", Duration = 232, Genre = "k-pop", Lyrics = "Ты в порядке Саня?" });
+            songs.Add(new Song { Title = "5 MACHINE", Duration = 522, Genre = "??", Lyrics = "...." });
+            songs.Add(new Song { Title = "6 Cancion del mariachi ", Duration = 500, Genre = "ohno", Lyrics = "lala2" });
+            songs.Add(new Song { Title = "7 San Francisco", Duration = 500, Genre = "pop4", Lyrics = "lala3" });
+            songs.Add(new Song { Title = "8 Have You Ever Seen The Rain", Duration = 500, Genre = "pop5", Lyrics = "lalads3" });
+            songs.Add(new Song { Title = "9 Ghost Riders In The Sky", Duration = 500, Genre = "pop6", Lyrics = "lala23" });
+            songs.Add(new Song { Title = "10 the end", Duration = 500, Genre = "pop7", Lyrics = " yey" });
+
+            foreach (Song p in songs)
+            {
+                Console.WriteLine(p.Title + " " + p.Duration + " " + p.Genre + " " + p.Lyrics);
+            }
+
+            return songs;
+        }
+
+        public void  Play(bool loop)// Heres Play*/////////////////////////////
+        {
+            AddSongs();
+           
             loop = false;
             int a=0;
             if (loop == false)
             {
 
-                 a = Songs.Length - 1;
+                 a = songs.Capacity - 1;
 
             }
             for (int i = 0; i < a; i++)
             {
 
-                //Console.WriteLine(songs[i].Title+ songs.Lyrics);
+                Console.WriteLine("Title: "+songs[i].Title+" /Lyrics: " + songs[i].Lyrics);
                 System.Threading.Thread.Sleep(2000);
 
             }
+
+           
         }
 
-        public  void Add (params string[] songs)
-        {
-            /*List<Player> songs = new List<Player>();
-            songs.Add(new Player { Title = "eye of a tiger", Duration = 500, Genre = "rock" });
-            songs.Add(new Player { Title = "smooth criminal", Duration = 500, Genre = "pop" });
-            songs.Add(new Player { Title = "ring of fire", Duration = 500, Genre = "jazz" });*/
-        }
-        
-        public  ArrayList  Add2()
-        {
+      
 
-            var songs = new ArrayList(); 
-            songs.Add(new Player { Title = "1 eye of a tiger", Duration = 500, Genre = "rock", Lyrics="lalalalalaalalalala" });
-            songs.Add(new Player { Title = "2 smooth criminal", Duration = 500, Genre = "pop", Lyrics = "lalalalalaalalalala" });
-            songs.Add(new Player { Title = "3 Kickapoo", Duration = 260, Genre = "rock", Lyrics = "lalalalalaalalalala" });
-            songs.Add(new Player { Title = "4 Гладкие Криминал", Duration = 232, Genre = "k-pop", Lyrics = "Ты в порядке Саня?" });
-            songs.Add(new Player { Title = "5 MEME MACHINE", Duration = 522, Genre = "??", Lyrics = "...." });
-            songs.Add(new Player { Title = "6 Cancion del mariachi ", Duration = 500, Genre = "ohno", Lyrics = "lala2" });
-            songs.Add(new Player { Title = "7 San Francisco", Duration = 500, Genre = "pop4", Lyrics = "lala3" });
-            songs.Add(new Player { Title = "8 Have You Ever Seen The Rain", Duration = 500, Genre = "pop5", Lyrics = "lalads3" });
-            songs.Add(new Player { Title = "9 Ghost Riders In The Sky", Duration = 500, Genre = "pop6", Lyrics = "lala23" });
-            songs.Add(new Player { Title = "10 the end", Duration = 500, Genre = "pop7", Lyrics = " yey" });
-
-            foreach (Player p in songs)
-            {
-                Console.WriteLine(p.Title+" "+p.Duration + " " + p.Genre + " " + p.Lyrics);
-            }
-            
-            return songs;
-        }
-
-        public static ArrayList Shuffle()
+        public List<Song> Shuffle()
         {
-            ArrayList songs = new ArrayList();
-            songs.Add(new Player { Title = "1 eye of a tiger", Duration = 500, Genre = "rock", Lyrics = "lalalalalaalalalala" });
-            songs.Add(new Player { Title = "2 smooth criminal", Duration = 500, Genre = "pop", Lyrics = "lalalalalaalalalala" });
-            songs.Add(new Player { Title = "3 Kickapoo", Duration = 260, Genre = "rock", Lyrics = "lalalalalaalalalala" });
-            songs.Add(new Player { Title = "4 Гладкие Криминал", Duration = 232, Genre = "k-pop", Lyrics = "Ты в порядке Саня?" });
-            songs.Add(new Player { Title = "5 MEME MACHINE", Duration = 522, Genre = "??", Lyrics = "...." });
-            songs.Add(new Player { Title = "6 Cancion del mariachi ", Duration = 500, Genre = "ohno", Lyrics = "lala2" });
-            songs.Add(new Player { Title = "7 San Francisco", Duration = 500, Genre = "pop4", Lyrics = "lala3" });
-            songs.Add(new Player { Title = "8 Have You Ever Seen The Rain", Duration = 500, Genre = "pop5", Lyrics = "lalads3" });
-            songs.Add(new Player { Title = "9 Ghost Riders In The Sky", Duration = 500, Genre = "pop6", Lyrics = "lala23" });
-            songs.Add(new Player { Title = "10 the end", Duration = 500, Genre = "pop7", Lyrics = " yey" });
-            foreach (Player p in songs)
-            {
-                Console.WriteLine(p.Title + " " + p.Duration + " " + p.Genre + " " + p.Lyrics);
-            }
-            //Все ещё не могу сделать, чтобы методы нормально у меня возвращали результат.
-            //Поэтому снова просто в самом методе  создавал  песни.
-            ArrayList songs_shuffled = new ArrayList();
+            AddSongs();
+            List<Song> songs_shuffled = new List<Song>();
 
             for (int j = 1; j <= songs.Capacity; j += 3)
             {
@@ -224,28 +204,24 @@ namespace Audio_player
             Console.WriteLine("После сортировки");
             Console.WriteLine("");
 
-            foreach (Player d in songs_shuffled)
+            foreach (Song d in songs_shuffled)
             {
 
                 Console.WriteLine(d.Title + " " + d.Duration + " " + d.Genre + " " + d.Lyrics);
             }
             return songs_shuffled;
-
-            //return songs;
-            //get { return songs; }
         }
        
         public  void ShowLyr()
         {
 
-            List<Player> songs = new List<Player>();
             //Add2();
 
-            songs.Add(new Player { Title = "eye of a tiger", Duration = 500, Genre = "rock", Lyrics = "lalalalalaalalalala" });
-            songs.Add(new Player { Title = "smooth criminal", Duration = 500, Genre = "pop", Lyrics = "lalalalalaalalalala" });
-            songs.Add(new Player { Title = "Kickapoo", Duration = 260, Genre = "rock", Lyrics = "lalalalalaalalalala" });
-            songs.Add(new Player { Title = "rdasf", Duration = 232, Genre = "jazzpunk", Lyrics = "lalalalalaalalalala" });
-            songs.Add(new Player { Title = "raaa", Duration = 522, Genre = "electro-swing", Lyrics = "lalalalalaalalalala" });
+            songs.Add(new Song { Title = "eye of a tiger", Duration = 500, Genre = "rock", Lyrics = "lalalalalaalalalala" });
+            songs.Add(new Song { Title = "smooth criminal", Duration = 500, Genre = "pop", Lyrics = "lalalalalaalalalala" });
+            songs.Add(new Song { Title = "Kickapoo", Duration = 260, Genre = "rock", Lyrics = "lalalalalaalalalala" });
+            songs.Add(new Song { Title = "rdasf", Duration = 232, Genre = "jazzpunk", Lyrics = "lalalalalaalalalala" });
+            songs.Add(new Song { Title = "raaa", Duration = 522, Genre = "electro-swing", Lyrics = "lalalalalaalalalala" });
 
             foreach (var item in songs)
             {
@@ -268,31 +244,16 @@ namespace Audio_player
 
         public  void SortByTitle() 
         {
-
-            //ArrayList songs = new ArrayList();
-            List<Player> songs = new List<Player>();
-            songs.Add(new Player { Title2 = "m1 eye of a tiger", Duration = 500, Genre = "rock", Lyrics = "lalalalalaalalalala" });
-            songs.Add(new Player { Title2 = "l2 smooth criminal", Duration = 500, Genre = "pop", Lyrics = "lalalalalaalalalala" });
-            songs.Add(new Player { Title2 = "k3 Kickapoo", Duration = 260, Genre = "rock", Lyrics = "lalalalalaalalalala" });
-            songs.Add(new Player { Title2 = "g4 Гладкие Криминал", Duration = 232, Genre = "k-pop", Lyrics = "Ты в порядке Саня?" });
-            songs.Add(new Player { Title2 = "f5 MEME MACHINE", Duration = 522, Genre = "??", Lyrics = "...." });
-            songs.Add(new Player { Title2 = "e6 Cancion del mariachi ", Duration = 500, Genre = "ohno", Lyrics = "lala2" });
-            songs.Add(new Player { Title2 = "d7 San Francisco", Duration = 500, Genre = "pop4", Lyrics = "lala3" });
-            songs.Add(new Player { Title2 = "c8 Have You Ever Seen The Rain", Duration = 500, Genre = "pop5", Lyrics = "lalads3" });
-            songs.Add(new Player { Title2 = "b9 Ghost Riders In The Sky", Duration = 500, Genre = "pop6", Lyrics = "lala23" });
-            songs.Add(new Player { Title2 = "a10 the end", Duration = 500, Genre = "pop7", Lyrics = " yey" });
-            foreach (Player o in songs)
+            foreach (Song o in songs)
             {
                 Console.WriteLine(o.Title + " " + o.Duration + " " + o.Genre + " " + o.Lyrics);
             }
             ArrayList songssorts = new ArrayList();
 
-            foreach (Player o in songs)
+            foreach (Song o in songs)
             {
-                songssorts.Add(Title2);
+                songssorts.Add(Title);
             }
-            //не рабоатает
-            //var t = songs.OrderBy(a => a.Title);
 
         }
 
